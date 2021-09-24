69a354131c69f22bdecdeddc7f54ac44fd2f472b
Christian Vadalà <chr.vadala@gmail.com>
2021-09-25 01:41:49 +0200

    initial import

---
+++ index.js
@@ -0,0 +1,138 @@
+
+const PARTITION_COUNT_DIFFERENCE_ERROR = "The number of partion can't change after setup()"
+const TOPIC_NOT_INITIALIZED = "This topic has not been initialized. Please call ensureTopic()"
+const GROUP_NOT_INITIALIZED = "This consumer has not been initialized. Please call ensureTopic()"
+
+export async function spyTopic(kafka, topic, groupIds = []) {
+    return new TopicSpy(kafka, topic, groupIds)
+}
+
+export class TopicSpy {
+    #kafka; #topic; #groupIds;
+
+    #topicOffsets = null;
+    #groupTopicOffsets = {};
+
+    constructor(kafka, topic, groupIds = []) {
+        this.#kafka = kafka;
+        this.#topic = topic;
+        this.#groupIds = groupIds;
+    }
+
+    async setup() {
+        const admin = await this.#getAdmin()
+
+        if (await this.#topicExists(admin)) {
+            this.#topicOffsets = await admin.fetchTopicOffsets(this.#topic)
+
+            for (const groupId of this.#groupIds) {
+                this.#groupTopicOffsets[groupId] = await admin.fetchOffsets({
+                    groupId, topic: this.#topic
+                })
+            }
+        }
+        await admin.disconnect()
+    }
+
+    async ensureTopicExists(timeout = null) {
+        const admin = await this.#getAdmin()
+
+        if (!await this.#topicExists(admin)) {
+            await admin.createTopics({
+                validateOnly: false,
+                waitForLeaders: true,
+                timeout: timeout ? timeout : 5000,
+                topics: [{
+                    topic: this.#topic,
+                }],
+            })
+        }
+        await admin.disconnect()
+    }
+
+    async ensureTopicDeleted(timeout = null) {
+        const admin = await this.#getAdmin()
+        if (await this.#topicExists(admin)) {
+            await admin.deleteTopics({
+                topics: [this.#topic],
+                timeout: timeout ? timeout : 5000,
+            })
+        }
+        await admin.disconnect()
+    }
+
+    async producedMessages(partition = null) {
+        const admin = await this.#getAdmin()
+        const topicOffsets = await admin.fetchTopicOffsets(this.#topic)
+
+        if (!this.#topicOffsets) {
+            throw new Error(TOPIC_NOT_INITIALIZED)
+        }
+
+        if (this.#topicOffsets.length !== topicOffsets.length) {
+            throw new Error(PARTITION_COUNT_DIFFERENCE_ERROR)
+        }
+
+        let tot = 0;
+        for (let i = 0; i < this.#topicOffsets.length; i++) {
+            tot += TopicSpy.offsetDelta(this.#topicOffsets[i].offset, topicOffsets[i].offset)
+        }
+
+        await admin.disconnect()
+
+        return tot
+    }
+
+    async messages() {
+        //TODO
+    }
+
+    async consumedMessagesByGroup(groupId, partition = null) {
+        if(!this.#groupIds.includes(groupId)){
+            throw new Error(GROUP_NOT_INITIALIZED)
+        }
+        const admin = await this.#getAdmin()
+
+        const oldGroupTopicOffsets = this.#groupTopicOffsets[groupId]
+        const currentGroupTopicOffsets = await admin.fetchOffsets({
+            groupId, topic: this.#topic
+        })
+
+        if (oldGroupTopicOffsets.length !== currentGroupTopicOffsets.length) {
+            throw new Error(PARTITION_COUNT_DIFFERENCE_ERROR)
+        }
+
+        let delta = 0;
+        for (let i = 0; i < oldGroupTopicOffsets.length; i++) {
+            delta += TopicSpy.offsetDelta(
+                oldGroupTopicOffsets[i].offset,
+                currentGroupTopicOffsets[i].offset
+            )
+        }
+
+        await admin.disconnect()
+        return delta
+    }
+
+    async pendingMessagesByGroup(groupId, partition = null) {
+        //TODO
+    }
+
+    async #getAdmin() {
+        const admin = this.#kafka.admin()
+        await admin.connect()
+        return admin
+    }
+
+    async #topicExists(admin) {
+        const topics = await admin.listTopics()
+        const exists = topics.includes(this.#topic)
+        return exists
+    }
+
+    static offsetDelta(offset1, offset2) {
+        const n1 = Number(offset1)
+        const n2 = Number(offset2)
+        return n2 - n1
+    }
+}
---
+++ index.test.js
@@ -0,0 +1,275 @@
+import { describe, expect, it, jest } from '@jest/globals'
+import { Kafka, logLevel } from 'kafkajs'
+import { TopicSpy } from '.';
+import waitForExpect from "wait-for-expect"
+
+const KAFKA_SERVER = process.env.KAFKA_SERVER;
+if (!KAFKA_SERVER) {
+    console.error('KAFKA_SERVER environment variable not found')
+    process.exit(1)
+}
+
+let kafka, admin;
+
+//when running a test suite is it useful to speed up group join
+const CONSUMER_TIMEOUT_DEFAULTS = {
+    sessionTimeout: 10_000,
+    rebalanceTimeout: 12_000,
+    heartbeatInterval: 500,
+}
+
+beforeAll(async () => {
+    kafka = new Kafka({
+        clientId: "tester",
+        brokers: [KAFKA_SERVER],
+        logLevel: logLevel.ERROR,
+        retry: {
+            restartOnFailure: async (error) => false
+        }
+    })
+
+    admin = kafka.admin()
+    await admin.connect()
+})
+
+afterAll(async () => {
+    await admin.disconnect()
+    admin = null
+})
+
+describe("setup", () => {
+    it("should construct and setup the component when topic exists", async () => {
+        const testTopic = randomString('topic')
+
+        await admin.createTopics({
+            validateOnly: false,
+            waitForLeaders: true,
+            topics: [{
+                topic: testTopic,
+            }],
+        })
+
+        const spiedTopic = new TopicSpy(kafka, testTopic)
+        await spiedTopic.setup()
+    })
+
+    it("should construct and setup the component when topic does NOT exist", async () => {
+        const testTopic = randomString('topic')
+        const spiedTopic = new TopicSpy(kafka, testTopic)
+        await spiedTopic.setup()
+    })
+
+    it("should construct and setup the component, consumer group included", async () => {
+        const testTopic = randomString('topic')
+        const testGroup1 = randomString('group1')
+        const testGroup2 = randomString('group2')
+
+        await admin.createTopics({
+            validateOnly: false,
+            waitForLeaders: true,
+            topics: [{
+                topic: testTopic,
+            }],
+        })
+        const consumer1 = kafka.consumer({ groupId: testGroup1 })
+        await consumer1.subscribe({ topic: testTopic })
+
+        const spiedTopic = new TopicSpy(kafka, testTopic, [testGroup1, testGroup2])
+        await spiedTopic.setup()
+
+        await consumer1.disconnect()
+    })
+})
+
+describe("ensureTopicExists", () => {
+    it("should create a new topic", async () => {
+        const testTopic = randomString('topic')
+
+        const spiedTopic = new TopicSpy(kafka, testTopic)
+
+        expect(await admin.listTopics())
+            .toEqual(expect.not.arrayContaining([testTopic]))
+
+        await spiedTopic.ensureTopicExists()
+
+        expect(await admin.listTopics())
+            .toEqual(expect.arrayContaining([testTopic]))
+    })
+
+    it("should not fail, even if the topic already exists", async () => {
+        const testTopic = randomString('topic')
+
+        const spiedTopic = new TopicSpy(kafka, testTopic)
+
+        await admin.createTopics({
+            validateOnly: false,
+            waitForLeaders: true,
+            topics: [{
+                topic: testTopic,
+            }],
+        })
+
+        await spiedTopic.ensureTopicExists()
+
+        expect(await admin.listTopics())
+            .toEqual(expect.arrayContaining([testTopic]))
+    })
+})
+
+describe("ensureTopicDeleted", () => {
+    it("should delete a topic", async () => {
+        const testTopic = randomString('topic')
+
+        await admin.createTopics({
+            validateOnly: false,
+            waitForLeaders: true,
+            topics: [{
+                topic: testTopic,
+            }],
+        })
+
+        expect(await admin.listTopics())
+            .toEqual(expect.arrayContaining([testTopic]))
+
+        const spiedTopic = new TopicSpy(kafka, testTopic)
+        await spiedTopic.ensureTopicDeleted()
+
+        expect(await admin.listTopics())
+            .toEqual(expect.not.arrayContaining([testTopic]))
+    })
+
+    it("should not fail, even if the topic does not exist", async () => {
+        const testTopic = randomString('topic')
+
+        const spiedTopic = new TopicSpy(kafka, testTopic)
+
+        expect(await admin.listTopics())
+            .toEqual(expect.not.arrayContaining([testTopic]))
+
+        await spiedTopic.ensureTopicDeleted()
+    })
+})
+
+describe("offsetDelta", () => {
+    it("should calculate delta on ordered offsets", () => {
+        expect(TopicSpy.offsetDelta("4", "4")).toBe(0)
+        expect(TopicSpy.offsetDelta("4", "5")).toBe(1)
+        expect(TopicSpy.offsetDelta("100", "200")).toBe(100)
+    })
+    it.todo("should calculate delta after an offset overflow")
+})
+
+describe("producedMessages", () => {
+    it("should count produced messages", async () => {
+        const testTopic = randomString('topic')
+
+        await admin.createTopics({
+            validateOnly: false,
+            waitForLeaders: true,
+            topics: [{
+                topic: testTopic,
+            }],
+        })
+
+        const producer = kafka.producer()
+        await producer.connect()
+        await producer.send({
+            topic: testTopic,
+            messages: [
+                { value: 'message-1' },
+                { value: 'message-2' },
+            ],
+        })
+
+        const spiedTopic = new TopicSpy(kafka, testTopic)
+        await spiedTopic.setup()
+
+        expect(await spiedTopic.producedMessages()).toBe(0)
+
+        await producer.send({
+            topic: testTopic,
+            messages: [
+                { value: 'message-3' },
+                { value: 'message-4' },
+                { value: 'message-5' },
+            ],
+        })
+
+        expect(await spiedTopic.producedMessages()).toBe(3)
+        await producer.disconnect()
+    })
+
+    it.todo("should work when the topic is created after setup")
+    it.todo("should throw exception when it found new partitions")
+    it.todo("should count messages on a specific partition")
+})
+
+describe("consumedMessagesByGroup", () => {
+    it("should calculate consumed message by a group", async () => {
+        const testTopic = randomString('topic')
+        const testGroup = randomString('group')
+        const handleMessage = jest.fn()
+
+        await admin.createTopics({
+            validateOnly: false,
+            waitForLeaders: true,
+            topics: [{
+                topic: testTopic,
+            }],
+        })
+
+        //init producer
+        const producer = kafka.producer()
+        await producer.connect()
+        await producer.send({
+            topic: testTopic,
+            messages: [
+                { value: 'message-1' },
+                { value: 'message-2' },
+                { value: 'message-3' },
+            ],
+        })
+
+        //init consumer
+        const consumer = kafka.consumer({ groupId: testGroup, ...CONSUMER_TIMEOUT_DEFAULTS })
+        await consumer.connect()
+        await consumer.subscribe({ topic: testTopic, fromBeginning: true })
+        await consumer.run({
+            eachMessage: handleMessage
+        })
+        await waitForExpect(() => {
+            expect(handleMessage).toHaveBeenCalledTimes(3);
+        })
+
+        const spiedTopic = new TopicSpy(kafka, testTopic, [testGroup])
+        await spiedTopic.setup()
+        expect(await spiedTopic.consumedMessagesByGroup(testGroup)).toBe(0)
+        await producer.send({
+            topic: testTopic,
+            messages: [
+                { value: 'message-4' },
+                { value: 'message-5' },
+            ],
+        })
+        await waitForExpect(() => {
+            expect(handleMessage).toHaveBeenCalledTimes(5);
+        })
+        
+        expect(await spiedTopic.consumedMessagesByGroup(testGroup)).toBe(2)
+        await producer.disconnect()
+        await consumer.disconnect()
+    }, 10_000)
+
+    it.todo("should calculate consumed message by a group - consumer does not exist on setup")
+})
+
+let i = 0;
+const randomString = prefix => {
+    const random = new Date().toISOString()
+        .replace(/[-:]/g, '')
+        .replace(/\..*/, '')
+
+    const topic = `${random}_${prefix}_${i++}`
+
+    return topic;
+}
