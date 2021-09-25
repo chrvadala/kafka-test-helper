ebb4835d7ba9d8afb59a42c07e3b0cc3ba851714
Christian Vadalà <chr.vadala@gmail.com>
2021-09-25 12:54:12 +0200

    reduces code duplication

--- index.test.js
+++ index.test.js
@@ -41,13 +41,7 @@ describe("setup", () => {
     it("should construct and setup the component when topic exists", async () => {
         const testTopic = randomString('topic')
 
-        await admin.createTopics({
-            validateOnly: false,
-            waitForLeaders: true,
-            topics: [{
-                topic: testTopic,
-            }],
-        })
+        await createTopic(testTopic)
 
         const spiedTopic = new TopicSpy(kafka, testTopic)
         await spiedTopic.setup()
@@ -64,13 +58,7 @@ describe("setup", () => {
         const testGroup1 = randomString('group1')
         const testGroup2 = randomString('group2')
 
-        await admin.createTopics({
-            validateOnly: false,
-            waitForLeaders: true,
-            topics: [{
-                topic: testTopic,
-            }],
-        })
+        await createTopic(testTopic)
         const consumer1 = kafka.consumer({ groupId: testGroup1 })
         await consumer1.subscribe({ topic: testTopic })
 
@@ -101,13 +89,7 @@ describe("ensureTopicExists", () => {
 
         const spiedTopic = new TopicSpy(kafka, testTopic)
 
-        await admin.createTopics({
-            validateOnly: false,
-            waitForLeaders: true,
-            topics: [{
-                topic: testTopic,
-            }],
-        })
+        await createTopic(testTopic)
 
         await spiedTopic.ensureTopicExists()
 
@@ -120,13 +102,7 @@ describe("ensureTopicDeleted", () => {
     it("should delete a topic", async () => {
         const testTopic = randomString('topic')
 
-        await admin.createTopics({
-            validateOnly: false,
-            waitForLeaders: true,
-            topics: [{
-                topic: testTopic,
-            }],
-        })
+        await createTopic(testTopic)
 
         expect(await admin.listTopics())
             .toEqual(expect.arrayContaining([testTopic]))
@@ -163,40 +139,25 @@ describe("producedMessages", () => {
     it("should count produced messages", async () => {
         const testTopic = randomString('topic')
 
-        await admin.createTopics({
-            validateOnly: false,
-            waitForLeaders: true,
-            topics: [{
-                topic: testTopic,
-            }],
-        })
+        await createTopic(testTopic)
 
-        const producer = kafka.producer()
-        await producer.connect()
-        await producer.send({
-            topic: testTopic,
-            messages: [
-                { value: 'message-1' },
-                { value: 'message-2' },
-            ],
-        })
+        await produceMessages(testTopic, [
+            { value: 'message-1' },
+            { value: 'message-2' },
+        ])
 
         const spiedTopic = new TopicSpy(kafka, testTopic)
         await spiedTopic.setup()
 
         expect(await spiedTopic.producedMessages()).toBe(0)
 
-        await producer.send({
-            topic: testTopic,
-            messages: [
-                { value: 'message-3' },
-                { value: 'message-4' },
-                { value: 'message-5' },
-            ],
-        })
+        await produceMessages(testTopic, [
+            { value: 'message-3' },
+            { value: 'message-4' },
+            { value: 'message-5' },
+        ])
 
         expect(await spiedTopic.producedMessages()).toBe(3)
-        await producer.disconnect()
     })
 
     it.todo("should work when the topic is created after setup")
@@ -210,25 +171,13 @@ describe("consumedMessagesByGroup", () => {
         const testGroup = randomString('group')
         const handleMessage = jest.fn()
 
-        await admin.createTopics({
-            validateOnly: false,
-            waitForLeaders: true,
-            topics: [{
-                topic: testTopic,
-            }],
-        })
+        await createTopic(testTopic)
 
-        //init producer
-        const producer = kafka.producer()
-        await producer.connect()
-        await producer.send({
-            topic: testTopic,
-            messages: [
-                { value: 'message-1' },
-                { value: 'message-2' },
-                { value: 'message-3' },
-            ],
-        })
+        await produceMessages(testTopic, [
+            { value: 'message-1' },
+            { value: 'message-2' },
+            { value: 'message-3' },
+        ])
 
         //init consumer
         const consumer = kafka.consumer({ groupId: testGroup, ...CONSUMER_TIMEOUT_DEFAULTS })
@@ -244,19 +193,16 @@ describe("consumedMessagesByGroup", () => {
         const spiedTopic = new TopicSpy(kafka, testTopic, [testGroup])
         await spiedTopic.setup()
         expect(await spiedTopic.consumedMessagesByGroup(testGroup)).toBe(0)
-        await producer.send({
-            topic: testTopic,
-            messages: [
-                { value: 'message-4' },
-                { value: 'message-5' },
-            ],
-        })
+        await produceMessages(testTopic, [
+            { value: 'message-4' },
+            { value: 'message-5' },
+        ])
+
         await waitForExpect(() => {
             expect(handleMessage).toHaveBeenCalledTimes(5);
         })
-        
+
         expect(await spiedTopic.consumedMessagesByGroup(testGroup)).toBe(2)
-        await producer.disconnect()
         await consumer.disconnect()
     }, 10_000)
 
@@ -272,4 +218,26 @@ const randomString = prefix => {
     const topic = `${random}_${prefix}_${i++}`
 
     return topic;
+}
+
+const createTopic = async (topic, numPartitions = 1, replicationFactor = 1) => {
+    await admin.createTopics({
+        validateOnly: false,
+        waitForLeaders: true,
+        topics: [{
+            topic,
+            numPartitions,
+            replicationFactor
+        }],
+    })
+}
+
+const produceMessages = async (topic, messages) => {
+    const producer = kafka.producer()
+    await producer.connect()
+    await producer.send({
+        topic,
+        messages,
+    })
+    await producer.disconnect()
 }
